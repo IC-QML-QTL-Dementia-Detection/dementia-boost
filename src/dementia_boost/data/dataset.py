fba344878ab8e91c1da5a07b4c226363e63fe0a0
Kaue Miziara <kauemiziara@gmail.com>
2026-03-17 20:20:27 -0300

    feat: implement `OasisDataset` base class to encapsulate OASIS-II data

---
+++ src/dementia_boost/data/dataset.py
@@ -0,0 +1,50 @@
+from collections.abc import Callable
+
+from torch import Tensor, load
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+
+class OasisDataset(Dataset):
+    """
+    Custom PyTorch Dataset to load the pre-processed OASIS tensors.
+    """
+
+    def __init__(
+        self,
+        file_path: str,
+        transform: Callable | None = None,
+    ) -> None:
+        """
+        Args:
+            `file_path`: Path to the .pt file containing the data and targets.
+            `transform`: Optional callable transform to apply to the data.
+        """
+        self.data, self.targets = load(file_path)
+        self.transform = transform
+
+    def __len__(self) -> int:
+        """
+        Returns the total number of samples in the dataset.
+        """
+        return len(self.data)
+
+    def __getitem__(self, idx: int) -> tuple[Tensor, int]:
+        """
+        Retrieves the image and label at the specified index.
+
+        Args:
+            `idx`: The index of the item to retrieve.
+
+        Returns:
+            A tuple containing the transformed image tensor and its label.
+        """
+        img = self.data[idx]
+        target = int(self.targets[idx])
+
+        img = transforms.ToPILImage()(img)
+
+        if self.transform:
+            img = self.transform(img)
+
+        return img, target
